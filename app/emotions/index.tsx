1a22f25c8d1c7300115f6f4b9d0081e36036faa2
bokeum <bokmail83@gmail.com>
2024-11-08 11:35:05 +0900

    감정 상태 UI 변경

--- app/emotions/index.tsx
+++ app/emotions/index.tsx
@@ -1,13 +1,11 @@
 // app/emotions/index.tsx
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
-import { Header } from '../../components/common/Header';
 import EmotionsSelector from '../../components/emotions/EmotionsSelector';
 
 export default function EmotionsMain() {
   return (
     <View style={styles.container}>
-      <Header title="나만의 감정 상태" showBackButton={true} titleColor="#000" />
       <EmotionsSelector />
     </View>
   );
